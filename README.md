817ca55f5b5e472546229f81851c6c878c65e475
undercloud <lodashes@gmail.com>
2019-04-01 21:41:34 +0400

    Initial commit

--- README.md
+++ README.md
@@ -1,7 +1,27 @@
 # exceptor
+
+[![Build Status](https://travis-ci.org/undercloud/exceptor.svg?branch=master)](https://travis-ci.org/undercloud/exceptor)
+
 Exception's flow
 
+## Install
+
+`composer require undercloud/exceptor`
+
+## Requirements
+
+PHP 5.4+
+
+## Description
+
+Provides a mechanism for catching errors and exceptions in the application flow. All built-in types of errors are transformed into exceptions, which greatly simplifies debugging. It also intercepts errors due to lack of memory and execution timeout.
+
+## Usage
+
 ```php
+// catch all
+error_reporting(E_ALL);
+
 use Undercloud\Exception\FlowHandler;
 
 (new FlowHandler(function(){
@@ -15,6 +35,30 @@ use Undercloud\Exception\FlowHandler;
         return $e;
     }
 }))->flow(function($e){
-  
+    // handle exception
 })
 ```
+
+## Type of Exceptions
+
+* All standard PHP's exceptions
+* CompileErrorException
+* CompileWarningException
+* CoreErrorException
+* CoreWarningException
+* DeprecatedException
+* NoticeException
+* ParseException
+* RecoverableErrorException
+* StrictException
+* UserDeprecatedException
+* UserErrorException
+* UserNoticeException
+* UserWarningException
+* WarningException
+* ExecutionTimeoutException
+* OutOfMemoryException
+
+## LICENSE
+
+MIT
--- tests/ExceptorTest.php
+++ tests/ExceptorTest.php
@@ -17,4 +17,72 @@ class ExceptorTest extends PHPUnit_Framework_TestCase
             $this->assertTrue($e instanceof NoticeException);
         });
     }
+
+    public function testWarning()
+    {
+        (new FlowHandler(function(){
+            try {
+                in_array(false,false);
+            } catch (Exception $e) {
+                return $e;
+            } catch (Throwable $e) {
+                return $e;
+            }
+        }))->flow(function($e){
+            $this->assertTrue($e instanceof WarningException);
+        });
+    }
+
+    public function testUserDeprecated()
+    {
+        (new FlowHandler(function(){
+            try {
+                trigger_error('', E_USER_DEPRECATED);
+            } catch (Exception $e) {
+                return $e;
+            } catch (Throwable $e) {
+                return $e;
+            }
+        }))->flow(function($e){
+            $this->assertTrue($e instanceof UserDeprecatedException);
+        });
+    }
+
+    public function testOutOfMemory()
+    {
+        (new FlowHandler(function(){
+            try {
+                $a = [];
+                while(true){
+                    $a[] = time();
+                }
+            } catch (Exception $e) {
+                return $e;
+            } catch (Throwable $e) {
+                return $e;
+            }
+        }))->flow(function($e){
+            $this->assertTrue($e instanceof OutOfMemoryException);
+        });
+    }
+
+    public function testExecutionTimeout()
+    {
+        set_time_limit(1);
+        (new FlowHandler(function(){
+            try {
+                $a = [];
+                while(true){
+                    $a[] = time();
+                    $a = [];
+                }
+            } catch (Exception $e) {
+                return $e;
+            } catch (Throwable $e) {
+                return $e;
+            }
+        }))->flow(function($e){
+            $this->assertTrue($e instanceof ExecutionTimeoutException);
+        });
+    }
 }
